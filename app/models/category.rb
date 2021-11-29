6a0198f7bccb2d2dca2109e6c7b0a0f401b3e8c0
greglobrigo <golivefun@gmail.com>
2021-11-30 01:30:56 +0800

    generated tasks scaffold and implemented association between tables

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+    has_many :tasks, dependent: :destroy
     validates :title, presence: true, length: { minimum: 4, maximum: 50 }, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_132406) do
+ActiveRecord::Schema.define(version: 2021_11_29_150909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.date "task_date"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
