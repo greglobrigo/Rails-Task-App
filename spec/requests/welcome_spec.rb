8dff6d7c6dfce4322fc7b9f6db4386cd183d629d
greglobrigo <golivefun@gmail.com>
2021-12-03 02:36:05 +0800

    added tests to category and task requests

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -1,11 +1,10 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Welcomes", type: :request do
-#   describe "GET /index" do
-#     it "returns http success" do
-#       get "/welcome/index"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-# end
+RSpec.describe "Welcomes", type: :request do
+  describe "GET /index" do
+    it "1. Returns http success on welcome page" do
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
