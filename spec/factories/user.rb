c83e638f49b0483e93e4c950c8cc96126baf5121
greglobrigo <golivefun@gmail.com>
2021-12-05 18:11:44 +0800

    added crud category tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
       email { "test@account1.com" }
       password { "1234567" }
     end
+
+    factory(:category) do
+      title { "test" }
+    end
   end 
 
   
--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -2,5 +2,55 @@ require 'rails_helper'
 
 RSpec.describe 'Category', type: :feature do
 
-    
+    before do
+        sign_in create(:user)
+      end
+
+    describe '#index' do
+        it '1. Should show all categories' do
+            visit '/categories'
+            expect(page).to have_content('Categories')
+            expect(page).to have_content('New Category')
+            expect(page).to have_content('Home')        
+        end
+    end
+
+    describe '#new' do
+        it '2. Should create a new category' do
+            visit '/categories'
+            click_link 'New Category'
+            fill_in 'Title', with: 'Test Category'
+            click_button 'Create Category'
+            expect(page).to have_content('Test Category')
+        end
+    end
+
+    describe '#edit' do
+        it '3. Should edit a category' do            
+            visit '/categories'
+            click_link 'New Category'
+            fill_in 'Title', with: 'Test Category'
+            click_button 'Create Category'
+            visit '/categories'
+            click_link 'Show'
+            click_link 'Edit Category'
+            fill_in 'Title', with: 'Test Category Edited'
+            click_button 'Update Category'
+            expect(page).to have_content('Test Category Edited')
+        end
+    end
+
+    describe '#destroy' do
+        it '4. Should delete a category' do
+            visit '/categories'
+            click_link 'New Category'
+            fill_in 'Title', with: 'Test Category'
+            click_button 'Create Category'
+            visit '/categories'
+            click_link 'Show'
+            click_link 'Delete Category'
+            expect(page).to_not have_content('Test Category')
+        end
+    end
+
 end
