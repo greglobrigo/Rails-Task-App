da06be772a9bbabfee3007305ac317ce5268502e
greglobrigo <golivefun@gmail.com>
2021-12-07 14:23:09 +0800

    set devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :categories do
     resources :tasks
   end
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_in' => 'devise/sessions#new'
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
